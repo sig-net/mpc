dc648cba2d0804abe1b5d5bfb25a67030f461e80
Daniyar Itegulov <ditegulov@gmail.com>
2023-03-30 17:20:32 +1100

    remote actors + web server

--- src/actor.rs
+++ src/actor.rs
@@ -1,205 +1,196 @@
-use actix::prelude::*;
-use rand::RngCore;
-use std::collections::{btree_map::Entry, BTreeMap};
+use futures::prelude::*;
+use futures::stream::FuturesUnordered;
+use ractor::{
+    concurrency::Duration, Actor, ActorProcessingErr, ActorRef, BytesConvertable, RpcReplyPort,
+};
+use ractor_cluster::RactorClusterMessage;
+use serde::{Deserialize, Serialize};
 use threshold_crypto::{PublicKeySet, SecretKeyShare, Signature, SignatureShare};
 
+const MPC_RECOVERY_GROUP: &str = "mpc-recovery";
+
 type NodeId = u64;
-type TaskId = u64;
 type Payload = Vec<u8>;
 
-// A task provided by an external user. Is uniquely identified by its id, but also contains
-// the payload itself just for the sake of simplicity (not having to store/request a separate
-// mapping).
-#[derive(PartialEq, PartialOrd, Eq, Ord, Clone)]
-struct Task {
-    id: TaskId,
-    payload: Payload,
-}
-
-// Assuming the same task, this is a collection of known signature shares provided by each
-// respective node.
-type SigShareSet = BTreeMap<NodeId, SignatureShare>;
-
-// The database schema that validator nodes use to store signature shares they receive from other
-// nodes (or generated themselves).
-type SigShareDatabase = BTreeMap<Task, SigShareSet>;
-
-// The database schema that validator nodes use to store signature shares they receive from other
-// nodes (or generated themselves).
-type SigDatabase = BTreeMap<Task, Signature>;
-
-#[derive(Clone)]
-struct SignedMsg {
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SignResponse {
     node_id: NodeId,
-    task: Task,
     sig_share: SignatureShare,
 }
 
-#[derive(Clone)]
-struct GotSignature {
-    task: Task,
-    sig: Signature,
+impl BytesConvertable for SignResponse {
+    fn into_bytes(self) -> Vec<u8> {
+        serde_json::to_vec(&self).unwrap()
+    }
+
+    fn from_bytes(bytes: Vec<u8>) -> Self {
+        serde_json::from_slice(&bytes).unwrap()
+    }
 }
 
-#[derive(Message, Clone)]
-#[rtype(result = "Option<NodeMessage>")]
-enum NodeMessage {
-    NewRequest(Payload),
-    SignedMsg(SignedMsg),
-    GotSignature(GotSignature),
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SignatureResponse {
+    pub sig: Signature,
 }
 
-struct NodeActor {
+impl BytesConvertable for SignatureResponse {
+    fn into_bytes(self) -> Vec<u8> {
+        serde_json::to_vec(&self).unwrap()
+    }
+
+    fn from_bytes(bytes: Vec<u8>) -> Self {
+        serde_json::from_slice(&bytes).unwrap()
+    }
+}
+
+#[derive(RactorClusterMessage, Debug)]
+pub enum NodeMessage {
+    #[rpc]
+    NewRequest(Payload, RpcReplyPort<SignatureResponse>),
+    #[rpc]
+    SignRequest(Payload, RpcReplyPort<SignResponse>),
+}
+
+pub struct NodeActor;
+
+pub struct NodeActorState {
     id: NodeId,
     pk_set: PublicKeySet,
     sk_share: SecretKeyShare,
-    pending_task_db: SigShareDatabase,
-    completed_task_db: SigDatabase,
 }
 
+#[async_trait::async_trait]
 impl Actor for NodeActor {
-    type Context = Context<Self>;
-}
+    // An actor has a message type
+    type Msg = NodeMessage;
+    // and (optionally) internal state
+    type State = NodeActorState;
+    // Startup initialization args
+    type Arguments = (NodeId, PublicKeySet, SecretKeyShare);
 
-impl NodeActor {
-    fn handle_signed_msg(&mut self, msg: SignedMsg) -> Option<NodeMessage> {
-        // TODO: run some check that the msg.task.payload makes sense, fail if not
-
-        // You are too late, we have already completed the signature
-        if self.completed_task_db.contains_key(&msg.task) {
-            return None;
-        }
-
-        // Check that this is a valid signature share
-        if !self
-            .pk_set
-            .public_key_share(msg.node_id)
-            .verify(&msg.sig_share, &msg.task.payload)
-        {
-            return None;
-        }
-
-        let sig_shares = self
-            .pending_task_db
-            .entry(msg.task.clone())
-            .or_insert_with(|| BTreeMap::new());
-        match sig_shares.entry(msg.node_id) {
-            Entry::Vacant(vacant_entry) => {
-                vacant_entry.insert(msg.sig_share);
-                if let Ok(sig) = self.pk_set.combine_signatures(sig_shares.iter()) {
-                    Some(NodeMessage::GotSignature(GotSignature {
-                        task: msg.task,
-                        sig,
-                    }))
-                } else {
-                    Some(NodeMessage::SignedMsg(SignedMsg {
-                        node_id: self.id,
-                        sig_share: self.sk_share.sign(&msg.task.payload),
-                        task: msg.task,
-                    }))
-                }
-            }
-            Entry::Occupied(occupied_entry) if occupied_entry.get() == &msg.sig_share => None,
-            Entry::Occupied(_) => {
-                println!("someone is lying to me :(");
-                None
-            }
-        }
+    async fn pre_start(
+        &self,
+        myself: ActorRef<Self>,
+        args: (NodeId, PublicKeySet, SecretKeyShare),
+    ) -> Result<Self::State, ActorProcessingErr> {
+        ractor::pg::join(MPC_RECOVERY_GROUP.to_string(), vec![myself.get_cell()]);
+        // create the initial state
+        Ok(NodeActorState {
+            id: args.0,
+            pk_set: args.1,
+            sk_share: args.2,
+        })
     }
 
-    fn handle_got_signature(&mut self, msg: GotSignature) -> Option<NodeMessage> {
-        if self.pk_set.public_key().verify(&msg.sig, &msg.task.payload) {
-            self.pending_task_db.remove(&msg.task);
-            self.completed_task_db.insert(msg.task, msg.sig);
-            // TODO: if this is the node that is connected to the user, then respond
-        }
-
-        None
-    }
-
-    fn handle_new_request(&mut self, payload: Payload) -> Option<NodeMessage> {
-        // TODO: run some check that the payload makes sense, fail if not
-
-        let mut rng = rand::thread_rng();
-        Some(NodeMessage::SignedMsg(SignedMsg {
-            node_id: self.id,
-            sig_share: self.sk_share.sign(&payload),
-            task: Task {
-                id: rng.next_u64(),
-                payload,
-            },
-        }))
-    }
-}
-
-impl Handler<NodeMessage> for NodeActor {
-    type Result = Option<NodeMessage>;
-
-    fn handle(&mut self, msg: NodeMessage, _ctx: &mut Context<Self>) -> Self::Result {
-        match msg {
-            NodeMessage::NewRequest(payload) => self.handle_new_request(payload),
-            NodeMessage::SignedMsg(msg) => self.handle_signed_msg(msg),
-            NodeMessage::GotSignature(msg) => self.handle_got_signature(msg),
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::actor::{NodeActor, NodeMessage};
-    use actix::prelude::*;
-    use std::collections::BTreeMap;
-    use threshold_crypto::SecretKeySet;
-
-    #[actix_rt::test]
-    async fn test_trio() -> anyhow::Result<()> {
-        let sk_set = SecretKeySet::random(1, &mut rand::thread_rng());
-        let pk_set = sk_set.public_keys();
-
-        let nodes = (0..3)
-            .map(|id| {
-                let sk_share = sk_set.secret_key_share(id);
-                NodeActor {
-                    id,
-                    pk_set: pk_set.clone(),
-                    sk_share,
-                    pending_task_db: BTreeMap::new(),
-                    completed_task_db: BTreeMap::new(),
-                }
-                .start()
-            })
+    // This is our main message handler
+    async fn handle(
+        &self,
+        _myself: ActorRef<Self>,
+        message: Self::Msg,
+        state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        let remote_actors = ractor::pg::get_members(&MPC_RECOVERY_GROUP.to_string())
+            .into_iter()
+            .filter(|actor| !actor.get_id().is_local())
+            .map(ActorRef::<Self>::from)
             .collect::<Vec<_>>();
 
-        let payload = vec![1u8, 2, 3];
-
-        let mut messages: Vec<NodeMessage> = Vec::new();
-        // send initial signing request
-        nodes[0]
-            .send(NodeMessage::NewRequest(payload.clone()))
-            .await?
-            .into_iter()
-            .for_each(|v| messages.push(v));
-
-        while !messages.is_empty() {
-            let msg = messages.pop().unwrap();
-
-            // propagate message to all nodes (including yourself, whatever)
-            for node in &nodes {
-                node.send(msg.clone())
-                    .await?
-                    .into_iter()
-                    .for_each(|v| messages.push(v));
+        match message {
+            NodeMessage::NewRequest(payload, reply) => {
+                state
+                    .handle_new_request(payload, reply, &remote_actors)
+                    .await
             }
+            NodeMessage::SignRequest(payload, reply) => state.handle_signed_msg(payload, reply),
+        };
+        Ok(())
+    }
+}
 
-            if let NodeMessage::GotSignature(msg) = msg {
-                println!("Got signature: {:?}", msg.sig);
-                assert!(pk_set.public_key().verify(&msg.sig, &payload));
+impl NodeActorState {
+    fn handle_signed_msg(&mut self, payload: Payload, reply: RpcReplyPort<SignResponse>) {
+        // TODO: run some check that the msg.task.payload makes sense, fail if not
+
+        println!("Got a sign request");
+
+        reply
+            .send(SignResponse {
+                node_id: self.id,
+                sig_share: self.sk_share.sign(payload),
+            })
+            .unwrap();
+    }
+
+    async fn handle_new_request(
+        &mut self,
+        payload: Payload,
+        reply: RpcReplyPort<SignatureResponse>,
+        remote_actors: &Vec<ActorRef<NodeActor>>,
+    ) {
+        // TODO: run some check that the payload makes sense, fail if not
+
+        let mut futures = Vec::new();
+        println!("Asking {} nodes", remote_actors.len());
+        for actor in remote_actors {
+            let future = actor
+                .call(
+                    |tx| NodeMessage::SignRequest(payload.clone(), tx),
+                    Some(Duration::from_millis(2000)),
+                )
+                .map(|r| r.map_err(ractor::RactorErr::from))
+                .map(|r| match r {
+                    Ok(ractor::rpc::CallResult::Success(ok_value)) => Ok(ok_value),
+                    Ok(cr) => Err(ractor::RactorErr::from(cr)),
+                    Err(e) => Err(e),
+                });
+            futures.push(future);
+        }
+
+        // create unordered collection of futures
+        let futures = futures.into_iter().collect::<FuturesUnordered<_>>();
+
+        // use collection as a stream, await only first *threshold* futures to complete
+        let mut first_t = futures
+            .take(self.pk_set.threshold())
+            .collect::<Vec<_>>()
+            .await
+            .into_iter()
+            .filter_map(|r| r.ok())
+            .collect::<Vec<_>>();
+
+        first_t.push(SignResponse {
+            node_id: self.id,
+            sig_share: self.sk_share.sign(&payload),
+        });
+
+        let mut sig_shares = Vec::new();
+        for sign_response in &first_t {
+            if self
+                .pk_set
+                .public_key_share(sign_response.node_id)
+                .verify(&sign_response.sig_share, &payload)
+            {
+                sig_shares.push((sign_response.node_id, &sign_response.sig_share));
+            } else {
+                println!(
+                    "Node {} sent me an invalid signature >:(",
+                    sign_response.node_id
+                )
             }
         }
 
-        // stop system and exit
-        System::current().stop();
-
-        Ok(())
+        if let Ok(sig) = self
+            .pk_set
+            .combine_signatures(sig_shares.clone().into_iter())
+        {
+            println!("Got full signature: {:?}", sig);
+            reply.send(SignatureResponse { sig }).unwrap();
+        } else {
+            println!(
+                "Expected to get {} shares, but only got {}",
+                self.pk_set.threshold() + 1,
+                sig_shares.len()
+            );
+        }
     }
 }
