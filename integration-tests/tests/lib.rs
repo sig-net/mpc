696d3bd3ff201b771f49b267b9f688d300a617f4
Serhii Volovyk <SergeyVolovyk@gmail.com>
2023-04-18 19:40:26 +0300

    recovery flow added to integration tests

--- integration-tests/tests/lib.rs
+++ integration-tests/tests/lib.rs
@@ -3,8 +3,7 @@ use bollard::Docker;
 // use docker::{redis::Redis, relayer::Relayer};
 use futures::future::BoxFuture;
 use mpc_recovery::msg::{
-    AddKeyRequest, AddKeyResponse, LeaderRequest, LeaderResponse, NewAccountRequest,
-    NewAccountResponse,
+    AddKeyRequest, LeaderRequest, LeaderResponse, NewAccountRequest, NewAccountResponse, AddKeyResponse,
 };
 use rand::{distributions::Alphanumeric, Rng};
 use std::time::Duration;
@@ -141,12 +140,12 @@ async fn test_basic_action() -> anyhow::Result<()> {
             // TODO: write a test with real token
             // "validToken" should triger test token verifyer and return success
             let id_token = "validToken".to_string();
-            let account_id: String = rand::thread_rng()
+            let account_id_rand: String = rand::thread_rng()
                 .sample_iter(&Alphanumeric)
                 .take(10)
                 .map(char::from)
                 .collect();
-            let account_id = format!("mpc-recovery-{}.testnet", account_id.to_lowercase());
+            let account_id = format!("mpc-recovery-{}.testnet", account_id_rand.to_lowercase());
 
             let user_public_key =
                 near_crypto::SecretKey::from_random(near_crypto::KeyType::ED25519)
@@ -162,7 +161,27 @@ async fn test_basic_action() -> anyhow::Result<()> {
                 })
                 .await?;
             assert_eq!(status_code, 200);
+
             assert!(matches!(new_acc_response, NewAccountResponse::Ok));
+
+            tokio::time::sleep(Duration::from_millis(20000)).await;
+
+            let new_user_public_key =
+                near_crypto::SecretKey::from_random(near_crypto::KeyType::ED25519)
+                    .public_key()
+                    .to_string();
+
+            let (status_code2, add_key_response) = ctx
+                .leader_node
+                .add_key(AddKeyRequest {
+                    account_id: account_id.clone(),
+                    id_token: id_token.clone(),
+                    public_key: new_user_public_key,
+                })
+                .await?;
+
+            assert_eq!(status_code2, 200);
+            assert!(matches!(add_key_response, AddKeyResponse::Ok));
 
             tokio::time::sleep(Duration::from_millis(120000)).await;
 
