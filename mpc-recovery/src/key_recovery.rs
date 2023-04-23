e7b4b028d1e8b748a2d9c023e9e6e1450c4f7a89
David Millar-Durrant <dmillardurrant@gmail.com>
2023-04-23 21:45:11 +0200

    Added remote public key fetching/building

--- mpc-recovery/src/key_recovery.rs
+++ mpc-recovery/src/key_recovery.rs
@@ -1,11 +1,19 @@
-use crate::primitives::InternalAccountId;
-use near_crypto::{PublicKey, SecretKey};
+use crate::{
+    primitives::InternalAccountId,
+    transaction::{call, to_dalek_public_key},
+};
+use multi_party_eddsa::protocols::aggsig::KeyAgg;
+use near_crypto::{ED25519PublicKey, PublicKey, SecretKey};
 
-pub fn get_user_recovery_pk(_id: InternalAccountId) -> PublicKey {
-    // TODO: use key derivation or other techniques to generate a key
-    "ed25519:3BUQYE4ZfQ6A94CqCtAbdLURxo4eHv2L8JjC2KiXXdFn"
-        .parse()
-        .unwrap()
+pub async fn get_user_recovery_pk(
+    client: &reqwest::Client,
+    sign_nodes: &Vec<String>,
+    id: InternalAccountId,
+) -> anyhow::Result<PublicKey> {
+    let res = call(client, sign_nodes, "public_key", id).await?;
+
+    let pk = KeyAgg::key_aggregation_n(&res, 0).apk;
+    to_dalek_public_key(&pk).map(|k| PublicKey::ED25519(ED25519PublicKey(*k.as_bytes())))
 }
 
 pub fn _get_user_recovery_sk(_id: InternalAccountId) -> SecretKey {
