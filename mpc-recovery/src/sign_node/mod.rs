4a1af0dc90c97772a5fb2adaff9ab3bc46c5984f
David Millar-Durrant <dmillardurrant@gmail.com>
2023-04-23 21:44:30 +0200

    Added perhaps excessive logging while debugging

--- mpc-recovery/src/sign_node/mod.rs
+++ mpc-recovery/src/sign_node/mod.rs
@@ -1,6 +1,7 @@
 use self::aggregate_signer::{NodeInfo, Reveal, SignedCommitment, SigningState};
 use crate::msg::SigShareRequest;
 use crate::oauth::{OAuthTokenVerifier, UniversalTokenVerifier};
+use crate::primitives::InternalAccountId;
 use crate::NodeId;
 use axum::{http::StatusCode, routing::post, Extension, Json, Router};
 use curv::elliptic::curves::{Ed25519, Point};
@@ -11,7 +12,7 @@ use tokio::sync::RwLock;
 
 pub mod aggregate_signer;
 
-#[tracing::instrument(level = "debug", skip(node_key))]
+#[tracing::instrument(level = "debug", skip(node_key, nodes_public_keys))]
 pub async fn run(
     our_index: NodeId,
     nodes_public_keys: Vec<Point<Ed25519>>,
@@ -44,6 +45,7 @@ pub async fn run(
         .route("/commit", post(commit::<UniversalTokenVerifier>))
         .route("/reveal", post(reveal))
         .route("/signature_share", post(signature_share))
+        .route("/public_key", post(public_key))
         .layer(Extension(state));
 
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
@@ -80,6 +82,11 @@ async fn commit<T: OAuthTokenVerifier>(
                 // TODO Restrict this payload
                 request.payload,
             );
+            match &response {
+                Ok(_) => tracing::debug!("Successful commitment"),
+                Err(e) => tracing::error!("Commitment payload failed: {}", e),
+            };
+
             (StatusCode::OK, Json(response))
         }
         Err(_) => {
@@ -101,8 +108,14 @@ async fn reveal(
         .await
         .get_reveal(state.node_info, request)
     {
-        Ok(r) => (StatusCode::OK, Json(Ok(r))),
-        Err(e) => (StatusCode::BAD_REQUEST, Json(Err(e))),
+        Ok(r) => {
+            tracing::debug!("Successful reveal");
+            (StatusCode::OK, Json(Ok(r)))
+        }
+        Err(e) => {
+            tracing::error!("Reveal failed: {}", e);
+            (StatusCode::BAD_REQUEST, Json(Err(e)))
+        }
     }
 }
 
@@ -117,7 +130,21 @@ async fn signature_share(
         .await
         .get_signature_share(state.node_info, request)
     {
-        Ok(r) => (StatusCode::OK, Json(Ok(r))),
-        Err(e) => (StatusCode::BAD_REQUEST, Json(Err(e))),
+        Ok(r) => {
+            tracing::debug!("Successful signature share");
+            (StatusCode::OK, Json(Ok(r)))
+        }
+        Err(e) => {
+            tracing::error!("Signature share failed: {}", e);
+            (StatusCode::BAD_REQUEST, Json(Err(e)))
+        }
     }
+}
+
+async fn public_key(
+    Extension(state): Extension<SignNodeState>,
+    Json(_request): Json<InternalAccountId>,
+) -> (StatusCode, Json<Result<Point<Ed25519>, String>>) {
+    // TODO lookup correct public key
+    (StatusCode::OK, Json(Ok(state.node_key.public_key)))
 }
