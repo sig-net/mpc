9600bfd47b248bb11a1af9ad2b7bf7068dc09455
Serhii Volovyk <SergeyVolovyk@gmail.com>
2024-02-06 14:19:59 +0200

    near conversions added (not working)

--- integration-tests/tests/multichain/actions/mod.rs
+++ integration-tests/tests/multichain/actions/mod.rs
@@ -2,13 +2,13 @@ pub mod wait_for;
 
 use crate::MultichainTestContext;
 
-use cait_sith::FullSignature;
+use k256::elliptic_curve::point::AffineCoordinates;
 use k256::elliptic_curve::sec1::FromEncodedPoint;
 use k256::{AffinePoint, EncodedPoint, Scalar, Secp256k1};
 use mpc_contract::RunningContractState;
 use mpc_recovery_node::kdf;
 use mpc_recovery_node::util::ScalarExt;
-use near_crypto::InMemorySigner;
+use near_crypto::{InMemorySigner, Secp256K1Signature};
 use near_jsonrpc_client::methods::broadcast_tx_async::RpcBroadcastTxAsyncRequest;
 use near_lake_primitives::CryptoHash;
 use near_primitives::transaction::{Action, FunctionCallAction, Transaction};
@@ -63,22 +63,97 @@ pub async fn single_signature_production(
     state: &RunningContractState,
 ) -> anyhow::Result<()> {
     let (payload, account, tx_hash) = request_sign(ctx).await?;
+    let (signature_big_r, signature_s) = wait_for::signature_responded(ctx, tx_hash).await?;
 
-    // Reconstract caith_sith::FullSignature
-    let (big_r, s) = wait_for::signature_responded(ctx, tx_hash).await?;
-    let signature = cait_sith::FullSignature::<Secp256k1> { big_r, s };
+    let mpc_pk = state.public_key.clone();
 
-    // Reconstruct caith_sith MPC PublicKey
-    let mut pk_bytes = vec![0x04];
-    pk_bytes.extend_from_slice(&state.public_key.as_bytes()[1..]);
-    let point = EncodedPoint::from_bytes(pk_bytes).unwrap();
-    let mpc_pk = AffinePoint::from_encoded_point(&point).unwrap();
+    let derivation_epsilon = kdf::derive_epsilon(account.id(), "test");
 
-    // Derive user PK
-    let epsilon = kdf::derive_epsilon(account.id(), "test");
-    let user_pk = kdf::derive_key(mpc_pk, epsilon);
-
-    assert!(signature.verify(&user_pk, &Scalar::from_bytes(&payload),));
+    check_signature_cait_sith(
+        &signature_big_r,
+        &signature_s,
+        &payload,
+        &derivation_epsilon,
+        &mpc_pk,
+    );
+    check_signature_near(
+        &signature_big_r,
+        &signature_s,
+        &payload,
+        &derivation_epsilon,
+        &mpc_pk,
+    );
 
     Ok(())
+}
+
+fn check_signature_cait_sith(
+    signature_big_r: &AffinePoint,
+    signature_s: &Scalar,
+    payload: &[u8; 32],
+    derivation_epsilon: &Scalar,
+    mpc_pk: &near_sdk::PublicKey,
+) {
+    let signature = cait_sith::FullSignature::<Secp256k1> {
+        big_r: *signature_big_r,
+        s: *signature_s,
+    };
+
+    let mpc_pk = near_sdk_pk_to_affine_point(mpc_pk);
+
+    let user_pk = kdf::derive_key(mpc_pk, *derivation_epsilon);
+
+    assert!(signature.verify(&user_pk, &Scalar::from_bytes(payload),));
+}
+
+fn check_signature_near(
+    signature_big_r: &AffinePoint,
+    signature_s: &Scalar,
+    payload: &[u8; 32],
+    derivation_epsilon: &Scalar,
+    mpc_pk: &near_sdk::PublicKey,
+) {
+    let signature = reconstruct_near_signature(signature_big_r, signature_s);
+
+    // Reconstract user PublicKey
+    let mpc_pk = near_sdk_pk_to_affine_point(mpc_pk);
+    let user_pk = kdf::derive_key(mpc_pk, *derivation_epsilon);
+    let user_pk = affine_point_to_near_pk(&user_pk);
+
+    assert!(signature.verify(payload, &user_pk));
+}
+
+fn near_sdk_pk_to_affine_point(pk: &near_sdk::PublicKey) -> AffinePoint {
+    let mut pk_bytes = vec![0x04];
+    pk_bytes.extend_from_slice(&pk.as_bytes()[1..]);
+    let point = EncodedPoint::from_bytes(pk_bytes).unwrap();
+    AffinePoint::from_encoded_point(&point).unwrap()
+}
+
+// TODO: fix
+fn reconstruct_near_signature(
+    signature_big_r: &AffinePoint,
+    signature_s: &Scalar,
+) -> near_crypto::Signature {
+    let signature_bytes: [u8; 65] = {
+        let mut signature_bytes = [0u8; 65];
+        signature_bytes[0] = signature_big_r.y_is_odd().unwrap_u8();
+        signature_bytes[1..33].copy_from_slice(&signature_big_r.x());
+        signature_bytes[33..65].copy_from_slice(&signature_s.to_bytes());
+        signature_bytes
+    };
+    near_crypto::Signature::SECP256K1(Secp256K1Signature::try_from(signature_bytes).unwrap())
+}
+
+// TODO: fix
+fn affine_point_to_near_pk(point: &AffinePoint) -> near_crypto::PublicKey {
+    let point_bytes = {
+        let mut point_bytes = [0u8; 64];
+        point_bytes[0..32].copy_from_slice(&point.x());
+        point_bytes[32..64].copy_from_slice(&point.x()); // TODO: definetelly not what we need
+        point_bytes
+    };
+    near_crypto::PublicKey::SECP256K1(
+        near_crypto::Secp256K1PublicKey::try_from(point_bytes).unwrap(),
+    )
 }
