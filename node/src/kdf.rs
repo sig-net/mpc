4b5d75c4f5f68d2759d9024d5c153f14e949a050
David Millar-Durrant <dmillardurrant@gmail.com>
2024-02-14 11:42:57 +1100

    We no longer use signer ID for this

--- node/src/kdf.rs
+++ node/src/kdf.rs
@@ -14,14 +14,14 @@ const EPSILON_DERIVATION_PREFIX: &str = "near-mpc-recovery v0.1.0 epsilon deriva
 // near-mpc-recovery with key derivation protocol vX.Y.Z.
 const DELTA_DERIVATION_PREFIX: &str = "near-mpc-recovery v0.1.0 delta derivation:";
 
-pub fn derive_epsilon(signer_id: &AccountId, path: &str) -> Scalar {
+pub fn derive_epsilon(predecessor_id: &AccountId, path: &str) -> Scalar {
     // TODO: Use a key derivation library instead of doing this manually.
     // https://crates.io/crates/hkdf might be a good option?
     //
     // ',' is ACCOUNT_DATA_SEPARATOR from nearcore that indicate the end
     // of the accound id in the trie key. We reuse the same constant to
     // indicate the end of the account id in derivation path.
-    let derivation_path = format!("{EPSILON_DERIVATION_PREFIX}{},{}", signer_id, path);
+    let derivation_path = format!("{EPSILON_DERIVATION_PREFIX}{},{}", predecessor_id, path);
     let mut hasher = Sha256::new();
     hasher.update(derivation_path);
     Scalar::from_bytes(&hasher.finalize())
