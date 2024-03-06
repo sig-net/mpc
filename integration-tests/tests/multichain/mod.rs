a228d2b90a78a1d029769f45ff9b44a788b6eac3
Phuong Nguyen <ChaoticTempest@users.noreply.github.com>
2024-03-06 10:29:50 -1000

    fix/test: derive recovery id for same address (#470)
    * Got a failing sig test
    
    * pk deserialization
    
    * passing sig verify
    
    * Fix imports
    
    * Fixed tests
    
    * Added more to unit test
    
    * naming
    
    * Added ethers test
    
    * Correct endian for etheres
    
    * Working signature verify check but on the one I copied
    
    * recover to same address
    
    * make verify pass
    
    * clenup
    
    * naming
    
    * fmt
    
    * added kdf::recovery_id
    
    * Added integration test for testing addr
    
    * assert signature fix
    
    * clippy
    
    * Address comments and clippy
    
    * Added back test_triples_persistence_for_generation
    
    * fmt
    
    ---------
    
    Co-authored-by: David Millar-Durrant <dmillardurrant@gmail.com>
    Co-authored-by: Serhii Volovyk <SergeyVolovyk@gmail.com>
    Co-authored-by: Xiangyi Zheng <xiangyi@near.org>

--- integration-tests/tests/multichain/mod.rs
+++ integration-tests/tests/multichain/mod.rs
@@ -2,9 +2,12 @@ pub mod actions;
 
 use crate::with_multichain_nodes;
 use actions::wait_for;
+use k256::elliptic_curve::point::AffineCoordinates;
 use mpc_recovery_integration_tests::env::containers::DockerClient;
+use mpc_recovery_node::kdf::{self, x_coordinate};
 use mpc_recovery_node::test_utils;
 use mpc_recovery_node::types::LatestBlockHeight;
+use mpc_recovery_node::util::{NearPublicKeyExt, ScalarExt};
 use test_log::test;
 
 #[test(tokio::test)]
@@ -56,6 +59,67 @@ async fn test_signature() -> anyhow::Result<()> {
             wait_for::has_at_least_triples(&ctx, 2).await?;
             wait_for::has_at_least_presignatures(&ctx, 2).await?;
             actions::single_signature_production(&ctx, &state_0).await
+        })
+    })
+    .await
+}
+
+#[test(tokio::test)]
+async fn test_key_derivation() -> anyhow::Result<()> {
+    with_multichain_nodes(3, |ctx| {
+        Box::pin(async move {
+            let state_0 = wait_for::running_mpc(&ctx, 0).await?;
+            assert_eq!(state_0.participants.len(), 3);
+            wait_for::has_at_least_triples(&ctx, 2).await?;
+            wait_for::has_at_least_presignatures(&ctx, 2).await?;
+
+            for _ in 0..5 {
+                let mpc_pk: k256::AffinePoint = state_0.public_key.clone().into_affine_point();
+                let (_, payload_hashed, account, tx_hash) = actions::request_sign(&ctx).await?;
+                let payload_hashed_rev = {
+                    let mut rev = payload_hashed;
+                    rev.reverse();
+                    rev
+                };
+                let sig = wait_for::signature_responded(&ctx, tx_hash).await?;
+
+                let hd_path = "test";
+                let derivation_epsilon = kdf::derive_epsilon(account.id(), hd_path);
+                let user_pk = kdf::derive_key(mpc_pk, derivation_epsilon);
+                let multichain_sig =
+                    kdf::into_eth_sig(&user_pk, &sig, k256::Scalar::from_bytes(&payload_hashed))
+                        .unwrap();
+
+                // start recovering the address and compare them:
+                let user_pk_x = kdf::x_coordinate::<k256::Secp256k1>(&user_pk);
+                let user_pk_y_parity = match user_pk.y_is_odd().unwrap_u8() {
+                    1 => secp256k1::Parity::Odd,
+                    0 => secp256k1::Parity::Even,
+                    _ => unreachable!(),
+                };
+                let user_pk_x =
+                    secp256k1::XOnlyPublicKey::from_slice(&user_pk_x.to_bytes()).unwrap();
+                let user_secp_pk =
+                    secp256k1::PublicKey::from_x_only_public_key(user_pk_x, user_pk_y_parity);
+                let user_addr = actions::public_key_to_address(&user_secp_pk);
+                let r = x_coordinate::<k256::Secp256k1>(&multichain_sig.big_r);
+                let s = multichain_sig.s;
+                let signature_for_recovery: [u8; 64] = {
+                    let mut signature = [0u8; 64];
+                    signature[..32].copy_from_slice(&r.to_bytes());
+                    signature[32..].copy_from_slice(&s.to_bytes());
+                    signature
+                };
+                let recovered_addr = web3::signing::recover(
+                    &payload_hashed_rev,
+                    &signature_for_recovery,
+                    multichain_sig.recovery_id as i32,
+                )
+                .unwrap();
+                assert_eq!(user_addr, recovered_addr);
+            }
+
+            Ok(())
         })
     })
     .await
--- node/src/kdf.rs
+++ node/src/kdf.rs
@@ -1,8 +1,13 @@
 use crate::types::PublicKey;
 use crate::util::ScalarExt;
+use anyhow::Context;
+use cait_sith::FullSignature;
 use hkdf::Hkdf;
+use k256::ecdsa::{RecoveryId, VerifyingKey};
+use k256::elliptic_curve::point::AffineCoordinates;
+use k256::elliptic_curve::sec1::ToEncodedPoint;
 use k256::elliptic_curve::CurveArithmetic;
-use k256::{Scalar, Secp256k1};
+use k256::{AffinePoint, Scalar, Secp256k1};
 use near_primitives::hash::CryptoHash;
 use near_primitives::types::AccountId;
 use sha2::{Digest, Sha256};
@@ -40,4 +45,59 @@ pub fn derive_delta(receipt_id: CryptoHash, entropy: [u8; 32]) -> Scalar {
 
 pub fn derive_key(public_key: PublicKey, epsilon: Scalar) -> PublicKey {
     (<Secp256k1 as CurveArithmetic>::ProjectivePoint::GENERATOR * epsilon + public_key).to_affine()
+}
+
+#[derive(Debug)]
+pub struct MultichainSignature {
+    pub big_r: AffinePoint,
+    pub s: Scalar,
+    pub recovery_id: u8,
+}
+
+// try to get the correct recovery id for this signature by brute force.
+pub fn into_eth_sig(
+    public_key: &k256::AffinePoint,
+    sig: &FullSignature<Secp256k1>,
+    msg_hash: Scalar,
+) -> anyhow::Result<MultichainSignature> {
+    let public_key = public_key.to_encoded_point(false);
+    let signature =
+        k256::ecdsa::Signature::from_scalars(x_coordinate::<k256::Secp256k1>(&sig.big_r), sig.s)
+            .context("cannot create signature from cait_sith signature")?;
+    let pk0 = VerifyingKey::recover_from_prehash(
+        &msg_hash.to_bytes(),
+        &signature,
+        RecoveryId::try_from(0).context("cannot create recovery_id=0")?,
+    )
+    .context("unable to use 0 as recovery_id to recover public key")?
+    .to_encoded_point(false);
+    if public_key == pk0 {
+        return Ok(MultichainSignature {
+            big_r: sig.big_r,
+            s: sig.s,
+            recovery_id: 0,
+        });
+    }
+
+    let pk1 = VerifyingKey::recover_from_prehash(
+        &msg_hash.to_bytes(),
+        &signature,
+        RecoveryId::try_from(1).context("cannot create recovery_id=1")?,
+    )
+    .context("unable to use 1 as recovery_id to recover public key")?
+    .to_encoded_point(false);
+    if public_key == pk1 {
+        return Ok(MultichainSignature {
+            big_r: sig.big_r,
+            s: sig.s,
+            recovery_id: 1,
+        });
+    }
+
+    anyhow::bail!("cannot use either recovery id (0 or 1) to recover pubic key")
+}
+
+/// Get the x coordinate of a point, as a scalar
+pub fn x_coordinate<C: cait_sith::CSCurve>(point: &C::AffinePoint) -> C::Scalar {
+    <C::Scalar as k256::elliptic_curve::ops::Reduce<<C as k256::elliptic_curve::Curve>::Uint>>::reduce_bytes(&point.x())
 }
