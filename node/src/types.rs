b224e3ef7ca23fd02e6853f1b869775eb8268714
ppca <xyzheng@uchicago.edu>
2024-03-27 16:58:37 -0700

    modify default timeout for triple (#537)

--- node/src/types.rs
+++ node/src/types.rs
@@ -13,10 +13,10 @@ use crate::gcp::value::{FromValue, IntoValue, Value};
 use crate::gcp::{DatastoreResult, GcpService, KeyKind};
 use crate::protocol::contract::ResharingContractState;
 
-/// Default timeout for triple/presig generation protocols. Times out after 10 minutes of being alive.
-pub const PROTOCOL_TRIPLE_TIMEOUT: Duration = Duration::from_secs(10 * 60);
+/// Default timeout for presig generation protocols. Times out after 20 minutes of being alive.
+pub const PROTOCOL_TRIPLE_TIMEOUT: Duration = Duration::from_secs(20 * 60);
 
-/// Default timeout for triple/presig generation protocols. Times out after 1 minute of being alive since this should be shorted lived.
+/// Default timeout for presig generation protocols. Times out after 1 minute of being alive since this should be shorted lived.
 pub const PROTOCOL_PRESIG_TIMEOUT: Duration = Duration::from_secs(60);
 
 /// Default timeout for signature generation protocol. Times out after 1 minute of being alive since this should be shorted lived.
