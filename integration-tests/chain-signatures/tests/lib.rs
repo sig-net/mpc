82de0988b59bd5c5da47ab69cfeff3e7b850b505
Phuong Nguyen <phuong@near.org>
2024-10-04 17:25:09 +0900

    Fix reshare test

--- integration-tests/chain-signatures/tests/lib.rs
+++ integration-tests/chain-signatures/tests/lib.rs
@@ -61,7 +61,7 @@ impl MultichainTestContext<'_> {
 
         self.nodes.start_node(&self.cfg, &node_account).await?;
         // Wait for new node to add itself as a candidate
-        tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+        tokio::time::sleep(tokio::time::Duration::from_secs(20)).await;
 
         // T number of participants should vote
         let participants = self.participant_accounts().await?;
@@ -76,7 +76,8 @@ impl MultichainTestContext<'_> {
             node_account.id(),
             self.cfg.threshold,
         )
-        .await?;
+        .await
+        .unwrap();
 
         let new_state = wait_for::running_mpc(self, Some(state.epoch + 1)).await?;
         assert_eq!(new_state.participants.len(), state.participants.len() + 1);
