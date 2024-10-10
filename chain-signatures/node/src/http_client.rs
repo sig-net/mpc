7fe23d1e67da0aec8955aa6339deebef1cb6c62f
Xiangyi Zheng <xiangyi@near.org>
2024-10-09 17:52:29 -0700

    increase timeout

--- chain-signatures/node/src/http_client.rs
+++ chain-signatures/node/src/http_client.rs
@@ -46,7 +46,7 @@ async fn send_encrypted<U: IntoUrl>(
             .post(url.clone())
             .header("content-type", "application/json")
             .json(&message)
-            .timeout(Duration::from_secs(1))
+            .timeout(Duration::from_secs(5))
             .send()
             .await
             .map_err(SendError::ReqwestClientError)?;
