8539fc8e14e70db45aea6c43af1ff3f6b765448f
Serhii Volovyk <SergeyVolovyk@gmail.com>
2024-10-11 18:06:01 +0300

    redis presignature storage used in presignature manager

--- chain-signatures/node/src/storage/presignature_storage.rs
+++ chain-signatures/node/src/storage/presignature_storage.rs
@@ -8,13 +8,12 @@ use url::Url;
 
 use crate::protocol::presignature::{Presignature, PresignatureId};
 
-// TODO: organize errors, get rid of connection with GCP
-type PresignatureResult<T> = std::result::Result<T, anyhow::Error>;
+type PresigResult<T> = std::result::Result<T, anyhow::Error>;
 pub type PresignatureStorageBox = Box<dyn PresignatureStorage + Send + Sync>;
 pub type LockPresignatureStorageBox = Arc<RwLock<PresignatureStorageBox>>;
 
-const FOREIGN_MAP_NAME: &'static str = "presignatures_foreign";
-const MINE_MAP_NAME: &'static str = "presignatures_mine";
+const PRESIGNATURES_MAP_NAME: &'static str = "presignatures";
+const MINE_SET_NAME: &'static str = "presignatures_mine";
 
 pub fn init(redis_url: Url) -> PresignatureStorageBox {
     Box::new(RedisPresignatureStorage::new(redis_url)) as PresignatureStorageBox
@@ -22,18 +21,14 @@ pub fn init(redis_url: Url) -> PresignatureStorageBox {
 
 #[async_trait]
 pub trait PresignatureStorage {
-    async fn insert_foreign(&mut self, presignature: Presignature) -> PresignatureResult<()>;
-    async fn insert_mine(&mut self, presignature: Presignature) -> PresignatureResult<()>;
-    async fn contains_foreign(&mut self, id: PresignatureId) -> PresignatureResult<bool>;
-    async fn contains_mine(&mut self, id: PresignatureId) -> PresignatureResult<bool>;
-    async fn remove_foreign(
-        &mut self,
-        id: PresignatureId,
-    ) -> PresignatureResult<Option<Presignature>>;
-    async fn remove_mine(&mut self, id: PresignatureId)
-        -> PresignatureResult<Option<Presignature>>;
-    async fn count_all(&mut self) -> PresignatureResult<usize>;
-    async fn count_mine(&mut self) -> PresignatureResult<usize>;
+    fn insert(&mut self, presignature: Presignature) -> PresigResult<()>;
+    fn insert_mine(&mut self, presignature: Presignature) -> PresigResult<()>;
+    fn contains(&mut self, id: &PresignatureId) -> PresigResult<bool>;
+    fn contains_mine(&mut self, id: &PresignatureId) -> PresigResult<bool>;
+    fn take(&mut self, id: &PresignatureId) -> PresigResult<Option<Presignature>>;
+    fn take_mine(&mut self) -> PresigResult<Option<Presignature>>;
+    fn count_all(&mut self) -> PresigResult<usize>;
+    fn count_mine(&mut self) -> PresigResult<usize>;
 }
 
 struct RedisPresignatureStorage {
@@ -46,79 +41,65 @@ impl RedisPresignatureStorage {
             redis_connection: redis::Client::open(redis_url.as_str())
                 .expect("Failed to connect to Redis")
                 .get_connection()
-                .expect("Failed to get connection"),
+                .expect("Failed to get Redis connection"),
         }
-    }
-
-    // This function is using a Lua script to read and remove
-    // a presignature in a single atomic operation
-    async fn remove_presignature(
-        &mut self,
-        hash_map_name: &str,
-        id: PresignatureId,
-    ) -> PresignatureResult<Option<Presignature>> {
-        let script = redis::Script::new(
-            r"
-                local value = redis.call('HGET', KEYS[1], ARGV[1])
-                redis.call('HDEL', KEYS[1], ARGV[1])
-                return value
-            ",
-        );
-        let presignature: Option<Presignature> = redis::pipe()
-            .invoke_script(script.key(hash_map_name).arg(id))
-            .query(&mut self.redis_connection)?;
-        Ok(presignature) as redis::RedisResult<Self>
     }
 }
 
+// Note: it is possible to use a Lua script to make all operations atomic
+// TODO: add logs and better error handling
 #[async_trait]
 impl PresignatureStorage for RedisPresignatureStorage {
-    async fn insert_foreign(&mut self, presignature: Presignature) -> PresignatureResult<()> {
+    fn insert(&mut self, presignature: Presignature) -> PresigResult<()> {
         self.redis_connection
-            .hset(FOREIGN_MAP_NAME, presignature.id, presignature)?;
+            .hset(PRESIGNATURES_MAP_NAME, presignature.id, presignature)?;
         Ok(())
     }
 
-    async fn insert_mine(&mut self, presignature: Presignature) -> PresignatureResult<()> {
-        self.redis_connection
-            .hset(MINE_MAP_NAME, presignature.id, presignature)?;
+    fn insert_mine(&mut self, presignature: Presignature) -> PresigResult<()> {
+        self.redis_connection.sadd(MINE_SET_NAME, presignature.id)?;
+        self.insert(presignature)?;
         Ok(())
     }
 
-    async fn contains_foreign(&mut self, id: PresignatureId) -> PresignatureResult<bool> {
-        let result: bool = self.redis_connection.hexists(FOREIGN_MAP_NAME, id)?;
+    fn contains(&mut self, id: &PresignatureId) -> PresigResult<bool> {
+        let result: bool = self.redis_connection.hexists(PRESIGNATURES_MAP_NAME, id)?;
         Ok(result)
     }
 
-    async fn contains_mine(&mut self, id: PresignatureId) -> PresignatureResult<bool> {
-        let result: bool = self.redis_connection.hexists(MINE_MAP_NAME, id)?;
+    fn contains_mine(&mut self, id: &PresignatureId) -> PresigResult<bool> {
+        let result: bool = self.redis_connection.sismember(MINE_SET_NAME, id)?;
         Ok(result)
     }
 
-    async fn remove_foreign(
-        &mut self,
-        id: PresignatureId,
-    ) -> PresignatureResult<Option<Presignature>> {
-        let result: Option<Presignature> = self.remove_presignature(FOREIGN_MAP_NAME, id).await?;
+    fn take(&mut self, id: &PresignatureId) -> PresigResult<Option<Presignature>> {
+        let result: Option<Presignature> =
+            self.redis_connection.hget(PRESIGNATURES_MAP_NAME, id)?;
+        match result {
+            Some(presignature) => {
+                self.redis_connection.hdel(PRESIGNATURES_MAP_NAME, id)?;
+                Ok(Some(presignature))
+            }
+            None => Ok(None),
+        }
+    }
+
+    fn take_mine(&mut self) -> PresigResult<Option<Presignature>> {
+        let id: Option<PresignatureId> = self.redis_connection.spop(MINE_SET_NAME)?;
+        match id {
+            Some(id) => self.take(&id),
+            None => Ok(None),
+        }
+    }
+
+    fn count_all(&mut self) -> PresigResult<usize> {
+        let result: usize = self.redis_connection.hlen(PRESIGNATURES_MAP_NAME)?;
         Ok(result)
     }
 
-    async fn remove_mine(
-        &mut self,
-        id: PresignatureId,
-    ) -> PresignatureResult<Option<Presignature>> {
-        let result: Option<Presignature> = self.remove_presignature(MINE_MAP_NAME, id).await?;
+    fn count_mine(&mut self) -> PresigResult<usize> {
+        let result: usize = self.redis_connection.scard(MINE_SET_NAME)?;
         Ok(result)
-    }
-
-    async fn count_all(&mut self) -> PresignatureResult<usize> {
-        let count: usize = self.redis_connection.hlen(FOREIGN_MAP_NAME)?;
-        Ok(count)
-    }
-
-    async fn count_mine(&mut self) -> PresignatureResult<usize> {
-        let count: usize = self.redis_connection.hlen(MINE_MAP_NAME)?;
-        Ok(count)
     }
 }
 
--- chain-signatures/node/src/web/mod.rs
+++ chain-signatures/node/src/web/mod.rs
@@ -141,9 +141,9 @@ async fn state(Extension(state): Extension<Arc<AxumState>>) -> Result<Json<State
             let triple_count = triple_manager_read.len();
             let triple_mine_count = triple_manager_read.my_len();
             let presignature_read = state.presignature_manager.read().await;
-            let presignature_count = presignature_read.len();
-            let presignature_mine_count = presignature_read.my_len();
-            let presignature_potential_count = presignature_read.potential_len();
+            let presignature_count = presignature_read.count_all().await;
+            let presignature_mine_count = presignature_read.count_mine().await;
+            let presignature_potential_count = presignature_read.potential_len().await;
             let participants = state.participants.keys_vec();
 
             Ok(Json(StateView::Running {
