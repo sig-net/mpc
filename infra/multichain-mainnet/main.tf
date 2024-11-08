669373d621238253609139a33f7123c495b88bcc
Serhii Volovyk <SergeyVolovyk@gmail.com>
2024-11-08 14:54:42 +0200

    fix tf mistake

--- infra/multichain-mainnet/main.tf
+++ infra/multichain-mainnet/main.tf
@@ -74,7 +74,6 @@ resource "google_service_account" "service_account" {
 
 resource "google_project_iam_member" "sa-roles" {
   for_each = toset([
-      "roles/.user",
       "roles/secretmanager.admin",
       "roles/storage.objectAdmin",
       "roles/iam.serviceAccountAdmin",
