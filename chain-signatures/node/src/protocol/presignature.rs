a57003018ee90cbdccced1131a3e74b3ea8e6053
Serhii Volovyk <SergeyVolovyk@gmail.com>
2024-11-19 17:05:58 +0200

    do not reinsert triples

--- chain-signatures/node/src/protocol/presignature.rs
+++ chain-signatures/node/src/protocol/presignature.rs
@@ -433,11 +433,6 @@ impl PresignatureManager {
                         participants = ?presig_participants.keys_vec(),
                         "running: the intersection of participants is less than the threshold"
                     );
-
-                    // Insert back the triples to be used later since this active set of
-                    // participants were not able to make use of these triples.
-                    triple_manager.insert_mine(triple0).await;
-                    triple_manager.insert_mine(triple1).await;
                 } else {
                     self.generate(
                         &presig_participants,
