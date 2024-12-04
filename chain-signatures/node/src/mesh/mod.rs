f9559e7e1083d1392c4998cab0ce59951a6cdaa7
Xiangyi Zheng <xyzheng@uhicago.edu>
2024-12-03 17:28:18 -0800

    make fn in Mesh private

--- chain-signatures/node/src/mesh/mod.rs
+++ chain-signatures/node/src/mesh/mod.rs
@@ -77,13 +77,13 @@ impl Mesh {
     }
 
     /// Participants that are active at the beginning of each protocol loop.
-    pub fn active_participants(&self) -> &Participants {
+    fn active_participants(&self) -> &Participants {
         &self.active_participants
     }
 
     /// Potential participants that are active at the beginning of each protocol loop. This will
     /// be empty if not in resharing state for the protocol
-    pub fn active_potential_participants(&self) -> &Participants {
+    fn active_potential_participants(&self) -> &Participants {
         &self.active_potential_participants
     }
 
@@ -92,24 +92,12 @@ impl Mesh {
         self.connections.potential_participants().await
     }
 
-    pub fn all_active_participants(&self) -> Participants {
-        let mut participants = self.active_participants.clone();
-        let active = self.active_potential_participants.keys_vec();
-        tracing::info!(?active, "Getting potentially active participants");
-        for (participant, info) in self.active_potential_participants.iter() {
-            if !participants.contains_key(participant) {
-                participants.insert(participant, info.clone());
-            }
-        }
-        participants
-    }
-
     /// Get active participants that have a stable connection. This is useful for arbitrary metrics to
     /// say whether or not a node is stable, such as a node being on track with the latest block height.
     ///
     /// NOTE: ping() will also ping our own node since everyone, including us, is stored in the contract state
     /// or `ProtocolState`, which eventually leads to checking our own stablitity.
-    pub async fn stable_participants(&self) -> Participants {
+    async fn stable_participants(&self) -> Participants {
         let mut stable = Participants::default();
         for (participant, info) in self.active_participants().iter() {
             if self.connections.is_participant_stable(participant).await {
@@ -144,7 +132,6 @@ impl Mesh {
         mesh_state: Arc<RwLock<MeshState>>,
     ) -> anyhow::Result<()> {
         let mut last_pinged = Instant::now();
-        tracing::info!("mesh is running");
         loop {
             if last_pinged.elapsed() > Duration::from_millis(300) {
                 if let Some(state) = contract_state.read().await.clone() {
