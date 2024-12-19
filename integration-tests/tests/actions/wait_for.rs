4c32d0038ed2cb6699e26cadd2327af3bce5e696
Phuong Nguyen <ChaoticTempest@users.noreply.github.com>
2024-12-19 12:16:30 -0800

    chore(refactor): better testing interface (#55)
    * Cleanup triples interface insert
    
    * Added triple store error
    
    * Cleaned presignature store
    
    * Upgrade bollard/testcontainers for better test interface
    
    * Added node cluster interface
    
    * Added SignAction interface
    
    * Added rogue respond to sign interface
    
    * Reformat all test with new interface
    
    * No more need for wait_for::running_mpc
    
    * Rename to signable
    
    * Remove unnecessary functions
    
    * signable rename
    
    * Remove unnecssary functions
    
    * Resolved comments
    
    * fmt
    
    * clippy
    
    * Rename vote/leave
    
    * Rename proper NodeConfig and NodeEnvConfig
    
    * Bump join to 20secs
    
    * clippy & fmt
    
    * Added wait for running/joining/resharing on per node
    
    * Fix mac build
    
    * Added candidate check
    
    * clippy and better err messages
    
    * Bump reshare test to 900secs
    
    * Better errors on vote_{join, leave}
    
    * Wait on finality final for running mpc
    
    * Fix offline test
    
    * clippy
    
    * Added test fixes
    
    ---------
    
    Co-authored-by: Phuong <entropy@Phuongs-MacBook-Pro.local>

--- integration-tests/tests/actions/wait_for.rs
+++ integration-tests/tests/actions/wait_for.rs
@@ -1,239 +1,19 @@
 use std::task::Poll;
 use std::time::Duration;
 
-use crate::actions;
-use crate::MultichainTestContext;
-
 use anyhow::Context;
+use backon::ConstantBuilder;
 use backon::Retryable;
-use backon::{ConstantBuilder, ExponentialBuilder};
 use cait_sith::FullSignature;
 use crypto_shared::SignatureResponse;
 use k256::Secp256k1;
-use mpc_contract::ProtocolContractState;
-use mpc_contract::RunningContractState;
-use mpc_node::web::StateView;
 use near_fetch::ops::AsyncTransactionStatus;
 use near_lake_primitives::CryptoHash;
 use near_primitives::errors::ActionErrorKind;
 use near_primitives::views::ExecutionOutcomeWithIdView;
 use near_primitives::views::ExecutionStatusView;
 use near_primitives::views::FinalExecutionStatus;
-use near_workspaces::Account;
 use std::collections::HashMap;
-use url::Url;
-
-pub async fn running_mpc<'a>(
-    ctx: &MultichainTestContext<'a>,
-    epoch: Option<u64>,
-) -> anyhow::Result<RunningContractState> {
-    let is_running = || async {
-        let state: ProtocolContractState = ctx
-            .rpc_client
-            .view(ctx.contract().id(), "state")
-            .await
-            .map_err(|err| anyhow::anyhow!("could not view state {err:?}"))?
-            .json()?;
-
-        match state {
-            ProtocolContractState::Running(running) => match epoch {
-                None => Ok(running),
-                Some(expected_epoch) if running.epoch >= expected_epoch => Ok(running),
-                Some(_) => {
-                    anyhow::bail!("running with an older epoch: {}", running.epoch)
-                }
-            },
-            _ => anyhow::bail!("not running"),
-        }
-    };
-    let err_msg = format!(
-        "mpc did not reach {} in time",
-        if epoch.is_some() {
-            "expected epoch"
-        } else {
-            "running state"
-        }
-    );
-    is_running
-        .retry(&ExponentialBuilder::default().with_max_times(6))
-        .await
-        .with_context(|| err_msg)
-}
-
-pub async fn has_at_least_triples<'a>(
-    ctx: &MultichainTestContext<'a>,
-    expected_triple_count: usize,
-) -> anyhow::Result<Vec<StateView>> {
-    let is_enough_triples = |id| {
-        move || async move {
-            let state_view: StateView = ctx
-                .http_client
-                .get(
-                    Url::parse(ctx.nodes.url(id))
-                        .unwrap()
-                        .join("/state")
-                        .unwrap(),
-                )
-                .send()
-                .await?
-                .json()
-                .await?;
-
-            tracing::debug!(
-                "has_at_least_triples state_view from {}: {:?}",
-                id,
-                state_view
-            );
-
-            match state_view {
-                StateView::Running { triple_count, .. }
-                    if triple_count >= expected_triple_count =>
-                {
-                    Ok(state_view)
-                }
-                StateView::Running { .. } => anyhow::bail!("node does not have enough triples yet"),
-                state => anyhow::bail!("node is not running {state:?}"),
-            }
-        }
-    };
-
-    let mut state_views = Vec::new();
-    for id in 0..ctx.nodes.len() {
-        let state_view = is_enough_triples(id)
-            .retry(&ExponentialBuilder::default().with_max_times(6))
-            .await
-            .with_context(|| format!("mpc node '{id}' failed to generate '{expected_triple_count}' triples before deadline"))?;
-        state_views.push(state_view);
-    }
-    Ok(state_views)
-}
-
-pub async fn has_at_least_mine_triples<'a>(
-    ctx: &MultichainTestContext<'a>,
-    expected_mine_triple_count: usize,
-) -> anyhow::Result<Vec<StateView>> {
-    let is_enough_mine_triples = |id| {
-        move || async move {
-            let state_view: StateView = ctx
-                .http_client
-                .get(
-                    Url::parse(ctx.nodes.url(id))
-                        .unwrap()
-                        .join("/state")
-                        .unwrap(),
-                )
-                .send()
-                .await?
-                .json()
-                .await?;
-
-            match state_view {
-                StateView::Running {
-                    triple_mine_count, ..
-                } if triple_mine_count >= expected_mine_triple_count => Ok(state_view),
-                StateView::Running { .. } => {
-                    anyhow::bail!("node does not have enough mine triples yet")
-                }
-                state => anyhow::bail!("node is not running {state:?}"),
-            }
-        }
-    };
-
-    let mut state_views = Vec::new();
-    for id in 0..ctx.nodes.len() {
-        let state_view = is_enough_mine_triples(id)
-            .retry(&ExponentialBuilder::default().with_max_times(15))
-            .await
-            .with_context(|| format!("mpc node '{id}' failed to generate '{expected_mine_triple_count}' triples before deadline"))?;
-        state_views.push(state_view);
-    }
-    Ok(state_views)
-}
-
-pub async fn has_at_least_presignatures<'a>(
-    ctx: &MultichainTestContext<'a>,
-    expected_presignature_count: usize,
-) -> anyhow::Result<Vec<StateView>> {
-    let is_enough_presignatures = |id| {
-        move || async move {
-            let state_view: StateView = ctx
-                .http_client
-                .get(
-                    Url::parse(ctx.nodes.url(id))
-                        .unwrap()
-                        .join("/state")
-                        .unwrap(),
-                )
-                .send()
-                .await?
-                .json()
-                .await?;
-
-            match state_view {
-                StateView::Running {
-                    presignature_count, ..
-                } if presignature_count >= expected_presignature_count => Ok(state_view),
-                StateView::Running { .. } => {
-                    anyhow::bail!("node does not have enough presignatures yet")
-                }
-                state => anyhow::bail!("node is not running {state:?}"),
-            }
-        }
-    };
-
-    let mut state_views = Vec::new();
-    for id in 0..ctx.nodes.len() {
-        let state_view = is_enough_presignatures(id)
-            .retry(&ExponentialBuilder::default().with_max_times(6))
-            .await
-            .with_context(|| format!("mpc node '{id}' failed to generate '{expected_presignature_count}' presignatures before deadline"))?;
-        state_views.push(state_view);
-    }
-    Ok(state_views)
-}
-
-pub async fn has_at_least_mine_presignatures<'a>(
-    ctx: &MultichainTestContext<'a>,
-    expected_mine_presignature_count: usize,
-) -> anyhow::Result<Vec<StateView>> {
-    let is_enough_mine_presignatures = |id| {
-        move || async move {
-            let state_view: StateView = ctx
-                .http_client
-                .get(
-                    Url::parse(ctx.nodes.url(id))
-                        .unwrap()
-                        .join("/state")
-                        .unwrap(),
-                )
-                .send()
-                .await?
-                .json()
-                .await?;
-
-            match state_view {
-                StateView::Running {
-                    presignature_mine_count,
-                    ..
-                } if presignature_mine_count >= expected_mine_presignature_count => Ok(state_view),
-                StateView::Running { .. } => {
-                    anyhow::bail!("node does not have enough mine presignatures yet")
-                }
-                state => anyhow::bail!("node is not running {state:?}"),
-            }
-        }
-    };
-
-    let mut state_views = Vec::new();
-    for id in 0..ctx.nodes.len() {
-        let state_view = is_enough_mine_presignatures(id)
-            .retry(&ExponentialBuilder::default().with_max_times(6))
-            .await
-            .with_context(|| format!("mpc node '{id}' failed to generate '{expected_mine_presignature_count}' presignatures before deadline"))?;
-        state_views.push(state_view);
-    }
-    Ok(state_views)
-}
 
 #[derive(Debug, thiserror::Error)]
 pub enum SignatureError {
@@ -251,8 +31,6 @@ pub enum WaitForError {
     Signature(SignatureError),
     #[error("Serde json error: {0}")]
     SerdeJson(String),
-    #[error("Parsing error")]
-    Parsing,
 }
 
 /// Used locally for testing to circumvent retrying on all errors. This will avoid retrying
@@ -299,26 +77,6 @@ pub async fn signature_responded(
             "Should not return more than one signature".to_string(),
         ))),
     }
-}
-
-pub async fn signature_payload_responded(
-    ctx: &MultichainTestContext<'_>,
-    account: Account,
-    payload: [u8; 32],
-    payload_hashed: [u8; 32],
-) -> Result<FullSignature<Secp256k1>, WaitForError> {
-    let is_signature_ready = || async {
-        let (_, _, _, status) =
-            actions::request_sign_non_random(ctx, account.clone(), payload, payload_hashed).await?;
-        let result = signature_responded(status).await;
-        if let Err(err) = &result {
-            println!("failed to produce signature: {err:?}");
-        }
-        result
-    };
-
-    let strategy = ConstantBuilder::default().with_max_times(3);
-    is_signature_ready.retry(&strategy).await
 }
 
 // Check that the rogue message failed
