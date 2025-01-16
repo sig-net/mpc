6cff4ab9852a7332df7230752f8c23453ac9ec8f
Bo Yao <by677@nyu.edu>
2025-01-16 09:16:36 +0800

    default to empty

--- chain-signatures/contract-eth/hardhat.config.js
+++ chain-signatures/contract-eth/hardhat.config.js
@@ -6,7 +6,7 @@ const { vars } = require("hardhat/config");
 // Go to https://infura.io, sign up, create a new API key
 // in its dashboard, and add it to the configuration variables
 // npx hardhat vars set INFURA_API_KEY 
-const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const INFURA_API_KEY = vars.get("INFURA_API_KEY", '');
 
 // Add your Sepolia account private key to the configuration variables
 // To export your private key from Coinbase Wallet, go to
@@ -14,7 +14,7 @@ const INFURA_API_KEY = vars.get("INFURA_API_KEY");
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // npx hardhat vars set SEPOLIA_PRIVATE_KEY 
-const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY", '');
 
 module.exports = {
   solidity: "0.8.27",
