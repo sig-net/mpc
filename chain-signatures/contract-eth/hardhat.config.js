3f84bd2e524023dee65e877f291ec948494b9825
Bo Yao <by677@nyu.edu>
2025-01-16 09:23:12 +0800

    key

--- chain-signatures/contract-eth/hardhat.config.js
+++ chain-signatures/contract-eth/hardhat.config.js
@@ -14,7 +14,7 @@ const INFURA_API_KEY = vars.get("INFURA_API_KEY", '');
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // npx hardhat vars set SEPOLIA_PRIVATE_KEY 
-const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY", '');
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY", 'ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80');
 
 module.exports = {
   solidity: "0.8.27",
