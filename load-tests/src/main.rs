da3bd5a4cfa87b084877c50ec0e07d6a229c36c4
Serhii Volovyk <SergeyVolovyk@gmail.com>
2025-01-27 19:27:29 +0200

    remove code duplication

--- load-tests/src/main.rs
+++ load-tests/src/main.rs
@@ -10,6 +10,8 @@ use tracing_subscriber::{filter, prelude::*};
 async fn main() -> Result<(), GooseError> {
     let stdout_log = tracing_subscriber::fmt::layer().pretty();
 
+    let sign_calls_per_account = 10;
+
     tracing_subscriber::registry()
         .with(stdout_log.with_filter(filter::LevelFilter::INFO))
         .init();
@@ -18,17 +20,8 @@ async fn main() -> Result<(), GooseError> {
         .register_scenario(
             scenario!("multichainSign")
                 .register_transaction(transaction!(prepare_user_credentials).set_sequence(1))
-                .register_transaction(transaction!(multichain_sign).set_sequence(2))
-                .register_transaction(transaction!(multichain_sign).set_sequence(3))
-                .register_transaction(transaction!(multichain_sign).set_sequence(4))
-                .register_transaction(transaction!(multichain_sign).set_sequence(5))
-                .register_transaction(transaction!(multichain_sign).set_sequence(6))
-                .register_transaction(transaction!(multichain_sign).set_sequence(7))
-                .register_transaction(transaction!(multichain_sign).set_sequence(8))
-                .register_transaction(transaction!(multichain_sign).set_sequence(9))
-                .register_transaction(transaction!(multichain_sign).set_sequence(10))
-                .register_transaction(transaction!(multichain_sign).set_sequence(11))
-                .register_transaction(transaction!(delete_user_account).set_sequence(12)),
+                .register_transaction(transaction!(multichain_sign).set_sequence(2).set_weight(sign_calls_per_account)?)
+                .register_transaction(transaction!(delete_user_account).set_sequence(3)),
         )
         .execute()
         .await?;
