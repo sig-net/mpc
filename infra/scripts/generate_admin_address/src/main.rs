b98c90bf806f35360e18d340ac415fbc2017deb5
Xiangyi Zheng <xyzheng@uhicago.edu>
2025-02-10 21:07:30 -0800

    script to generate admin address

---
+++ infra/scripts/generate_admin_address/src/main.rs
@@ -0,0 +1,21 @@
+use mpc_keys::hpke;
+use crypto_shared::{self, derive_epsilon_admin_address_eth, derive_key, x_coordinate, ScalarExt};
+use integration_tests::actions;
+
+fn main() {
+    let (sk, pk) = hpke::generate();
+    let derivation_path = "sig-network-eth-contract-balance-gov";
+    let epsilon = derive_epsilon_admin_address_eth(derivation_path);
+    let derived_pk = derive_key(pk, epsilon);
+    let derived_pk_x = x_coordinate(&derived_pk);
+    let derived_pk_y_parity = match derived_pk.y_is_odd().unwrap_u8() {
+        1 => secp256k1::Parity::Odd,
+        0 => secp256k1::Parity::Even,
+        _ => unreachable!(),
+    };
+    let derived_pk_x = secp256k1::XOnlyPublicKey::from_slice(&derived_pk_x.to_bytes()).unwrap();
+    let derived_secp_pk =
+        secp256k1::PublicKey::from_x_only_public_key(derived_pk_x, derived_pk_y_parity);
+    let derived_addr = actions::public_key_to_address(&derived_secp_pk);
+    println!("Derived address: {}", derived_addr);
+}
