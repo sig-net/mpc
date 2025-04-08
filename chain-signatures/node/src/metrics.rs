fc47361eee60ec1f0e234e0fe5866215e190e0ba
Xiangyi Zheng <xyzheng@uhicago.edu>
2025-04-08 13:43:12 +0200

    start bucket from 0.01s

--- chain-signatures/node/src/metrics.rs
+++ chain-signatures/node/src/metrics.rs
@@ -640,7 +640,7 @@ pub(crate) static INDEXER_DELAY: LazyLock<HistogramVec> = LazyLock::new(|| {
         "multichain_indexer_delay_secs",
         "Delay between block time of the request and the time a request gets indexed",
         &["chain", "node_account_id"],
-        Some(exponential_buckets(0.0, 1.5, 20).unwrap()),
+        Some(exponential_buckets(0.01, 1.5, 30).unwrap()),
     )
     .unwrap()
 });
