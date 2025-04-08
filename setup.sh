2859933be9094605b702032caad37992053abfe8
Phuong <phuong@sig.network>
2025-04-08 15:52:43 -0700

    Make setup.sh only run on integration-tests

--- setup.sh
+++ setup.sh
@@ -7,16 +7,17 @@ export TARGET_DIR=$ROOT_DIR/target
 CARGO_CMD_ARGS="$@"
 CARGO_BUILD_INDENT="            "
 
-echo "${CARGO_BUILD_INDENT} running MPC build script"
-# add additional features if we're benchmarking:
-if echo $CARGO_CMD_ARGS | grep -q "bench"; then
-    FEATURES="--features bench"
+if echo $CARGO_PKG_NAME | grep -q "integration-tests"; then
+    echo "${CARGO_BUILD_INDENT} running MPC build script"
+    # add additional features if we're benchmarking:
+    if echo $CARGO_CMD_ARGS | grep -q "bench"; then
+        FEATURES="--features bench"
+    fi
+
+    set --
+    set -e
+    . $ROOT_DIR/build-contract.sh $FEATURES
+    cargo build -p mpc-node --release $FEATURES
 fi
-
-set --
-set -e
-. $ROOT_DIR/build-contract.sh $FEATURES
-cargo build -p mpc-node --release $FEATURES
-
 
 exec $CARGO_CMD_ARGS
