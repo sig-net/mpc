c71b3fd0d396e5ba047e11ef9eef51ee8a5f9fbe
Serhii Volovyk <SergeyVolovyk@gmail.com>
2025-04-18 18:16:38 +0300

    use ramping-arrival-rate in lt

--- infra/loadtests/smoketest.js
+++ infra/loadtests/smoketest.js
@@ -2,37 +2,43 @@ import http from 'k6/http';
 import { group } from 'k6';
 
 export const options = {
-  stages: [
-    { duration: '2m', target: 1},  // 1 virtual users
-    { duration: '2m', target: 3},
-    { duration: '2m', target: 1},
-    { duration: '2m', target: 3},
-    { duration: '2m', target: 1},
-  ],
+  scenarios: {
+    ramping_smoke: {
+      executor: 'ramping-arrival-rate',
+      startRate: 1,
+      timeUnit: '1s',
+      preAllocatedVUs: 10,
+      maxVUs: 100,
+      stages: [
+        { duration: '2m', target: 1 },
+        { duration: '2m', target: 3 },
+        { duration: '2m', target: 1 },
+        { duration: '2m', target: 3 },
+        { duration: '2m', target: 1 },
+      ],
+    },
+  },
   thresholds: {
-    http_req_failed: ['rate<0.03'], // http errors should be less than 1%
-    http_req_duration: ['p(95)<1500'], // 95 percent of response times must be below 1500ms
+    http_req_failed: ['rate<0.03'],
+    http_req_duration: ['p(95)<1500'],
   },
 };
 
 export default function () {
-  group(`${__ENV.ENVIRONMENT} EVM`, function() {
-    let evmContract = {"contractAddress":`${__ENV.EVM_CONTRACT}`} // dev contract EVM
-    
+  group(`${__ENV.ENVIRONMENT} EVM`, function () {
+    let evmContract = { "contractAddress": `${__ENV.EVM_CONTRACT}` }
     let response_evm = http.post('https://contract-pinger-mainnet-1094058868047.europe-west1.run.app/evm_no_check', JSON.stringify(evmContract), {
       headers: { 'Content-Type': 'application/json' },
-    }) 
-    console.log("EVM Dev: " + response_evm)
+    })
+    console.log(`EVM Dev: Status ${response_evm.status}, Body: ${response_evm.body}`);
 
   })
 
-  group(`${__ENV.ENVIRONMENT} solana`, function() {
-    let solContract = {"contractAddress":`${__ENV.SOL_CONTRACT}`} // dev contract SOL
-    
+  group(`${__ENV.ENVIRONMENT} solana`, function () {
+    let solContract = { "contractAddress": `${__ENV.SOL_CONTRACT}` }
     let response_sol = http.post('https://contract-pinger-mainnet-1094058868047.europe-west1.run.app/solana_no_check', JSON.stringify(solContract), {
       headers: { 'Content-Type': 'application/json' },
     })
-  
-    console.log("SOL Dev: " + response_sol)
+    console.log(`SOL Dev: Status ${response_sol.status}, Body: ${response_sol.body}`);
   });
 }
