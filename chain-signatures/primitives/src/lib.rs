0350ae23702c6e1355a975f1cc27724148b07cfb
Serge Kovbasiuk <isserge@protonmail.com>
2026-06-20 10:27:56 +0200

    feat: add `StateManager` trait

--- chain-signatures/primitives/src/lib.rs
+++ chain-signatures/primitives/src/lib.rs
@@ -4,6 +4,7 @@ mod chain;
 mod crypto;
 mod events;
 mod requests;
+mod traits;
 
 pub use backlog::{Checkpoint, CheckpointDigest, ConsensusCheckpointDigest, PendingTx};
 pub use bidirectional::{
@@ -16,6 +17,7 @@ pub use crypto::{
 };
 pub use events::{ChainEvent, ExecutionOutcome, SignatureRespondedEvent};
 pub use requests::{IndexedSignRequest, SignKind};
+pub use traits::StateManager;
 
 pub const LATEST_MPC_KEY_VERSION: u32 = 1;
 pub const LEGACY_MPC_KEY_VERSION_0: u32 = 0;
