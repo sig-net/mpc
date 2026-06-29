43d491a7806ee41f4ab2343779013169455783f2
Serge Kovbasiuk <isserge@protonmail.com>
2026-06-29 12:24:31 +0200

    feat: add utility functions for request ID computation and payload hashing to `indexer-core`

--- chain-signatures/indexer-core/src/lib.rs
+++ chain-signatures/indexer-core/src/lib.rs
@@ -3,7 +3,9 @@
 mod indexer;
 mod state;
 mod telemetry;
+mod utils;
 
 pub use indexer::{ChainIndexer, ChainStream};
 pub use state::StateManager;
 pub use telemetry::{ChainTelemetry, NoopChainTelemetry};
+pub use utils::{compute_request_id, hash_payload};
