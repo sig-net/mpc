7d89b80d5f05886995ab06fa3e60645abdf15595
Serge Kovbasiuk <isserge@protonmail.com>
2026-07-02 16:53:26 +0200

    refactor: change module visibility for indexer and related modules

--- chain-signatures/chain-ethereum/src/lib.rs
+++ chain-signatures/chain-ethereum/src/lib.rs
@@ -1,15 +1,16 @@
 pub mod abi;
 mod client;
 mod config;
-pub mod indexer;
-pub mod indexer_eth_direct_rpc;
+mod indexer;
+mod indexer_eth_direct_rpc;
 #[cfg(feature = "helios")]
 pub mod indexer_eth_helios;
+// TODO: publisher client should be merged with indexer client
 pub mod publisher;
-pub mod respond_bidirectional;
+mod respond_bidirectional;
 #[cfg(test)]
-pub mod test_utils;
-pub mod util;
+mod test_utils;
+mod util;
 
 pub use config::EthConfig;
 pub use indexer::EthereumStream;
