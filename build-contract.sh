ca606ed6b7f4fe6af44bdf3ca7633d10a550b591
Phuong N. <phuong@sig.network>
2026-07-28 16:52:41 -0700

    add cfg args to build-contract.sh

--- build-contract.sh
+++ build-contract.sh
@@ -2,5 +2,5 @@
 
 cargo +1.93.0 near build non-reproducible-wasm \
     --manifest-path chain-signatures/contract/Cargo.toml \
-    --no-abi --env 'RUSTFLAGS=-C link-arg=--allow-undefined' \
+    --no-abi --env 'RUSTFLAGS=--cfg near -C link-arg=--allow-undefined' \
     $@
