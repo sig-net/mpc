58a97704d092d4ce3e67eb543cb050374dcf5b1f
Serge <isserge@protonmail.com>
2026-08-24 12:33:45 +0000

    refactor(solana): extract event handling (#1166)

--- chain-signatures/chain-solana/src/lib.rs
+++ chain-signatures/chain-solana/src/lib.rs
@@ -1,9 +1,11 @@
 mod client;
 mod config;
+mod events;
 mod indexer;
 mod utils;
 
 pub use anchor_lang::prelude::Pubkey;
 pub use client::SolanaClient;
 pub use config::SolConfig;
+pub use events::{emit_events, extract_tx_signature};
 pub use indexer::SolanaIndexer;
